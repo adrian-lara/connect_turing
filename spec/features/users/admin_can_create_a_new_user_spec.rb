02c90f3cb002e69754c64e0322f2cff7c0b4ab55
Adrian Lara <adrianblara@gmail.com>
2017-10-21 22:05:04 -0600

    changed looking for possibilities

--- spec/features/users/admin_can_create_a_new_user_spec.rb
+++ spec/features/users/admin_can_create_a_new_user_spec.rb
@@ -7,18 +7,18 @@ describe "When an admin visits '/users/new', enters user information, and clicks
     fill_in "user[name]", with: "Adrian Lara"
     fill_in "user[slack]", with: "adrian-lara"
     fill_in "user[email]", with: "adrianblara@gmail.com"
-    select "Mentor", from: "user[looking_for][]"
-    #TODO wut is up with the []????????????^^^^
-    # save_and_open_page
+    select "Mentor", from: "user[looking_for]"
     fill_in "user[about_me]", with: "I like to learn and help others learn where I can."
     fill_in "user[availability_notes]", with: "I'm pretty flexible!"
     click_on "Create User"
 
+    save_and_open_page
     expect(User.count).to eq(1)
     expect(User.last.name).to eq("Adrian Lara")
     expect(User.last.slack).to eq("adrian-lara")
     expect(User.last.availability_notes).to eq("I'm pretty flexible!")
     expect(current_path).to eq(users_path)
     expect(page).to have_link("Adrian Lara")
+    expect(page).to have_content("Mentor")
   end
 end
