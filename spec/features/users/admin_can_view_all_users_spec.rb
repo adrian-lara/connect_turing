3c2d1a6159bc8835c6164bfad81e3df353d1584e
Adrian Lara <adrianblara@gmail.com>
2017-10-22 00:27:53 -0600

    update index feature to reflect admin story

--- spec/features/users/admin_can_view_all_users_spec.rb
+++ spec/features/users/admin_can_view_all_users_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
-describe "When a visitor visits '/users'" do
+describe "When an admin visits '/users'" do
   it "a list of users and several details is shown on the page" do
     users = create_list(:user, 4)
     visit users_path
 
     expect(page).to have_link(users[0].name)
+    expect(page).to have_link("Delete", href: user_path(users[0]))
+    expect(page).to have_link("Edit", href: edit_user_path(users[0]))
     expect(page).to have_link(users[1].name)
+    expect(page).to have_link("Delete", href: user_path(users[1]))
+    expect(page).to have_link("Edit", href: edit_user_path(users[1]))
     expect(page).to have_content(users[2].slack)
     expect(page).to have_content(users[3].slack)
     expect(page).to have_content("Mentor")
