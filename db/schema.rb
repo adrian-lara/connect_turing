a3c011681d33fff0e4427ef7b284aeddbffdecff
Adrian Lara <adrianblara@gmail.com>
2017-10-22 19:20:37 -0600

    location table added to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021213557) do
+ActiveRecord::Schema.define(version: 20171023011858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,6 +31,9 @@ ActiveRecord::Schema.define(version: 20171021213557) do
     t.datetime "last_login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "location_id"
+    t.index ["location_id"], name: "index_users_on_location_id"
   end
 
+  add_foreign_key "users", "locations"
 end
