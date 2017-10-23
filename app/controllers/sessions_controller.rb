6b4beb0cb1b4a8b080318c2c2814ee136cde12f4
Adrian Lara <adrianblara@gmail.com>
2017-10-23 15:40:23 -0600

    flash messages added to log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      flash[:notice] = "Log In Successful!"
       redirect_to user_path(user)
     else
+      flash[:notice] = "Log In Failed!"
       render :new
     end
   end
@@ -16,6 +18,7 @@ class SessionsController < ApplicationController
   def destroy
     session.clear
 
+    flash[:notice] = "Log Out Successful!"
     redirect_to root_path
   end
 end
