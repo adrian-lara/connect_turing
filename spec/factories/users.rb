2e61d7a35ee93f6b01aa3ba4b852ef20de7919c7
Adrian Lara <adrianblara@gmail.com>
2017-10-23 14:35:03 -0600

    user log in/log out test written, and fix typo in user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "username-#{n}" }
-    sequence(:password) { |n| "passwoord-#{n}" }
+    sequence(:password) { |n| "password-#{n}" }
     sequence(:name) { |n| "User Name #{n}" }
     sequence(:slack) { |n| "Slack Handle #{n}" }
     sequence(:looking_for) { |n| ["Mentor", "Mentee"].sample }
---
+++ spec/features/users/guest_can_log_in_and_log_out_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "When a guest visits root, clicks Log In, submits information, and clicks Log Out" do
+  it "the user has logged in, viewed their show page, logged out, and is redirected to root" do
+    location = create(:location)
+    user = create(:user, location: location)
+    visit root_path
+
+    click_on "Log In"
+    expect(current_path).to eq(new_session_path)
+
+    fill_in 'user[username]', with: "username-1"
+    fill_in 'user[password]', with: "password-1"
+    click_on "Sign In"
+
+    expect(session[:user_id]).to eq(1)
+    expect(current_path).to eq(user_path(user))
+
+    click_on "Log Out"
+
+    expect(session[:user_id]).to be(nil)
+    expect(current_path).to eq(root_path)
+  end
+end
