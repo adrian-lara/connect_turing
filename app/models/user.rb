6c515c184b26de14a479e537a865ce155c2bde39
Adrian Lara <adrianblara@gmail.com>
2017-10-23 01:26:49 -0600

    many-to-many self join association set up... finally

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
 
   belongs_to :location
 
-  has_many :mentees, { through: :mentor_mentees, foreign_key: :mentor_id, class_name: "User" }
-  has_many :mentors, { through: :mentor_mentees, foreign_key: :mentee_id, class_name: "User" }
+  has_many :mentee_list, foreign_key: :mentor_id, class_name: "MentorMentee"
+  has_many :mentees, through: :mentee_list, source: :mentee
+
+  has_many :mentor_list, foreign_key: :mentee_id, class_name: "MentorMentee"
+  has_many :mentors, through: :mentor_list, source: :mentor
 
   def last_active
     return updated_at if last_login.nil?
