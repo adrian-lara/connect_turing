cc6ec9a3ab890f61d8784a7e23d7d4ccc28cb3e8
Adrian Lara <adrianblara@gmail.com>
2017-10-24 16:03:16 -0600

    admin can see the correct nav-bar

---
+++ spec/features/users/admin/admin_can_see_admin_links_in_nav_bar_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "When an admin visits admin users index" do
+  it "the admin sees admin relevant links" do
+    admin = User.create(username: 'admin',
+                        password: 'pass',
+                        name: "Person",
+                        role: 1
+                       )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit admin_users_path
+
+    expect(page).to have_link("Connect Turing", href: admin_users_path)
+    expect(page).to have_link("Log Out")
+    expect(page).not_to have_link("Sign In")
+    expect(page).not_to have_link("Log In")
+  end
+end
