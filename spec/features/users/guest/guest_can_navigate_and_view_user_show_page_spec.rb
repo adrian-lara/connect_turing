90ae0850670f45d57b52352e85de7a5ecae7708a
Adrian Lara <adrianblara@gmail.com>
2017-10-26 11:00:17 -0600

    guest can navigate and view user show page

---
+++ spec/features/users/guest/guest_can_navigate_and_view_user_show_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "When a guest visits the user index, clicks on a UserName link" do
+  it "the guest can now see the user show page" do
+    location = create(:location)
+    user = create(:user, location: location)
+
+    visit users_path
+    click_on "#{user.name}"
+
+    expect(page).to have_content(user.username)
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user.slack)
+    expect(page).to have_content(user.email)
+    expect(page).to have_content(user.location.description)
+    expect(page).to have_content(user.gender_pronoun)
+    expect(page).to have_content(user.looking_for)
+    expect(page).to have_content(user.about_me)
+    expect(page).to have_content(user.availability_notes)
+    expect(page).to have_content("Since Last Activity: ")
+    expect(page).not_to have_link("Edit")
+    expect(page).not_to have_link("Delete")
+  end
+end
