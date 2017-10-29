5c7c8552b0a6b4676cfc10cb7709749aacda994e
Adrian Lara <adrianblara@gmail.com>
2017-10-29 13:36:15 -0600

    refactor views to isolate index-content view logic into one area (to be resused)

--- spec/features/users/default/default_user_can_view_their_user_show_page_spec.rb
+++ spec/features/users/default/default_user_can_view_their_user_show_page_spec.rb
@@ -4,11 +4,13 @@ describe "When a default user" do
 
   before(:each) do
     @location = create(:location)
-    @users = create_list(:user, 2, location: @location)
+    @users = create_list(:user, 3, location: @location)
+    MentorMentee.create(mentor: @users[1], mentee: @users[0], status: 1)
+    MentorMentee.create(mentor: @users[0], mentee: @users[2], status: 1)
   end
 
-  describe "visits their own user show page" do
-    it "the visitor sees all of their information as well as an edit and delete link" do
+  describe "visits another user's show page" do
+    it "the default user sees all of the information without an edit and delete link" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@users[0])
 
       visit user_path(@users[1])
@@ -28,8 +30,8 @@ describe "When a default user" do
     end
   end
 
-  describe "visits another user's show page" do
-    it "the visitor sees all of their information as well as an edit and delete link" do
+  describe "visits their own user show page" do
+    it "the default user sees all of their information as well as an edit and delete link" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@users[0])
 
       visit user_path(@users[0])
@@ -46,6 +48,16 @@ describe "When a default user" do
       expect(page).to have_content("Since Last Activity: ")
       expect(page).to have_link("Edit")
       expect(page).to have_link("Delete")
+    end
+
+    it "the default user can see accepted mentor and mentee relationships" do
+      expect(page).to have_link(@users[0].mentors.first.name)
+      expect(page).to have_content(@users[0].mentors.first.slack)
+      expect(page).to have_content(@users[0].mentors.first.looking_for)
+
+      expect(page).to have_link(@users[0].mentees.first.name)
+      expect(page).to have_content(@users[0].mentees.first.slack)
+      expect(page).to have_content(@users[0].mentees.first.looking_for)
     end
   end
 
