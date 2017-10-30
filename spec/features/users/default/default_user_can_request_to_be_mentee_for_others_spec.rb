aaf1dba4d579bf8e623c6846a7606f038b3ea544
Adrian Lara <adrianblara@gmail.com>
2017-10-29 20:47:46 -0600

    default user sees show page test renamed more generally. and feature test written to find mentee request link

---
+++ spec/features/users/default/default_user_can_request_to_be_mentee_for_others_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "When a default user visits another user's show page" do
+  before(:each) do
+    @location = create(:location)
+    @users = create_list(:user, 2, location: @location)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@users[0])
+  end
+
+  it "the user can see the Request to be Mentor link" do
+    visit user_path(@users[1])
+
+    expect(page).to have_link("Request to be Mentor")
+  end
+end
